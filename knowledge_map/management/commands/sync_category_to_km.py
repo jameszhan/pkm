ad4c4551f6d68877b1fcdad57afac9380f76519a
James Zhan <zhiqiangzhan@gmail.com>
2023-09-18 03:12:33 +0800

    load Computational Theory

--- knowledge_map/management/commands/sync_category_to_km.py
+++ knowledge_map/management/commands/sync_category_to_km.py
@@ -21,7 +21,7 @@ class Command(BaseCommand):
             self.stdout.write(self.style.SUCCESS(f'{"创建" if created else "忽略"} {r.name}({r.slug}-{r.id})'))
             km_catalogs[r.slug] = r
 
-            if category.parent is not None and category.parent.slug in km_catalogs:
+            if created and category.parent is not None and category.parent.slug in km_catalogs:
                 r.parents.create(parent=km_catalogs[category.parent.slug])
                 self.stdout.write(self.style.SUCCESS(f'创建关系 {r.slug} <- {category.parent.slug}'))
 
