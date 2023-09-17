79c9f5b55895ffc26c779f6d7c43114eb4dff7db
James Zhan <zhiqiangzhan@gmail.com>
2023-09-18 02:12:03 +0800

    add eval expression

--- knowledge_map/functions.py
+++ knowledge_map/functions.py
@@ -1,5 +1,10 @@
+import json
+import logging
+import traceback
 from django.core.cache import cache
 from .models import Category, CategoryRelation
+
+logger = logging.getLogger(__name__)
 
 
 def get_all_root_categories():
@@ -16,7 +21,7 @@ def collect_category_tree(root_category, parent_slug=None, collector=None):
         collector = []
 
     unique_slug = f"{parent_slug}/{root_category.slug}" if parent_slug else root_category.slug
-    catalog_dict = {
+    category_dict = {
         'id': unique_slug,
         'parent': parent_slug if parent_slug else '#',
         'text': root_category.name,
@@ -29,14 +34,16 @@ def collect_category_tree(root_category, parent_slug=None, collector=None):
             'slug': root_category.slug,
         },
     }
-    collector.append(catalog_dict)
+    collector.append(category_dict)
 
     children = root_category.children.all()
     if len(children) == 0:
-        catalog_dict['type'] = 'leaf'
+        category_dict['type'] = 'leaf'
+
     for relation in children:
-        child_catalog = relation.child
-        collect_category_tree(child_catalog, unique_slug, collector)
+        child_category = relation.child
+        if eval_category_relation_context(relation, parent_slug):
+            collect_category_tree(child_category, unique_slug, collector)
         if relation.context:
             collector[-1]['context'] = relation.context
 
@@ -46,7 +53,7 @@ def collect_category_tree(root_category, parent_slug=None, collector=None):
 def get_category_tree(root_category, parent_slug=None):
     unique_slug = f"{parent_slug}/{root_category.slug}" if parent_slug else root_category.slug
 
-    catalog_dict = {
+    category_dict = {
         'id': root_category.id,
         'name': root_category.name,
         'unique_slug': unique_slug,
@@ -58,12 +65,34 @@ def get_category_tree(root_category, parent_slug=None):
     child_relations = root_category.children_relations.all()
 
     for relation in child_relations:
-        child_catalog = relation.child
-        child_dict = get_category_tree(child_catalog, unique_slug)
+        child_category = relation.child
+        child_dict = get_category_tree(child_category, unique_slug)
 
         if relation.context:
             child_dict['context'] = relation.context
 
-        catalog_dict['children'].append(child_dict)
+        category_dict['children'].append(child_dict)
 
-    return catalog_dict
+    return category_dict
+
+
+def eval_category_relation_context(relation, parent_slug):
+    if relation.context:
+        try:
+            context_config = json.loads(relation.context)
+            if 'expression' in context_config:
+                expression = context_config['expression']
+                ret = eval(expression, {}, {'parent_slug': parent_slug,
+                                            'slug': relation.parent.slug,
+                                            'child_slug': relation.child.slug})
+                # print(f'expression "{expression}" with locals {parent_slug}|{relation.parent.slug}'
+                #       f'|{relation.child.slug} and get {ret}')
+                if not ret:
+                    return False
+        except Exception as e:
+            logger.error(f"eval expression error: {e}", exc_info=True)
+            # traceback.print_exc()
+    return True
+
+
+
