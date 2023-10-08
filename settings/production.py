cfc96e6b4f42fc8b5c1005282b404eeb9260b888
James Zhan <zhiqiangzhan@gmail.com>
2023-10-08 13:27:15 +0800

    ADD file server

--- settings/production.py
+++ settings/production.py
@@ -2,6 +2,8 @@ from .base import *
 
 DEBUG = False
 
+FILE_SERVER = os.getenv('FILE_SERVER')
+
 STATIC_URL = 'https://dl.zizhizhan.com:8443/pkm/'
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-$nm6iy@q2hs1h9884y#sbjr6x*^@ys)*0gi4o*at$+8^4pwc6c')
