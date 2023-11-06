20c067327813edc37b86bf27a5e624ec0904d116
James Zhan <zhiqiangzhan@gmail.com>
2023-11-07 03:48:08 +0800

    upgrade to 0.0.55

--- webfs/views.py
+++ webfs/views.py
@@ -25,6 +25,24 @@ def date_hierarchy(model, year=None, month=None, day=None):
     return date_filters
 
 
+def date_hierarchy2(queryset, year=None, month=None, day=None):
+    date_filters = {
+        'year': year,
+        'month': month,
+        'day': day,
+    }
+
+    if year and month:
+        date_filters['days'] = queryset.filter(created_time__year=year,
+                                               created_time__month=month).dates('created_time', 'day')
+    elif year:
+        date_filters['months'] = queryset.filter(created_time__year=year).dates('created_time', 'month')
+    else:
+        date_filters['years'] = queryset.dates('created_time', 'year')
+
+    return date_filters
+
+
 @login_required
 def pdf_files(request, series_slug=None):
     files = (PDFUniqueFile.objects.prefetch_related('managed_files').prefetch_related('tags').all()
@@ -77,6 +95,53 @@ def pdf_files(request, series_slug=None):
 
 
 @login_required
+def pdf_files_by_resource_type(request, resource_type):
+    files = (PDFUniqueFile.objects.prefetch_related('managed_files').prefetch_related('tags')
+             .filter(resource_type=resource_type.upper()).all().order_by('-file_size'))
+
+    q = request.GET.get('q', None)
+    if q:
+        files = files.filter(Q(name__icontains=q) | Q(digest__icontains=q))
+
+    tag_slugs = request.GET.getlist('tag')
+    if tag_slugs:
+        tagged_tags = FileTag.objects.filter(slug__in=tag_slugs)
+        files = (files.filter(tags__in=tagged_tags).annotate(distinct_tags=Count('tags', distinct=True))
+                 .filter(distinct_tags=len(tagged_tags)))
+    else:
+        tagged_tags = []
+
+    year = request.GET.get('year')
+    month = request.GET.get('month')
+    day = request.GET.get('day')
+    if year and month and day:
+        files = files.filter(created_time__year=year, created_time__month=month, created_time__day=day)
+    elif year and month:
+        files = files.filter(created_time__year=year, created_time__month=month)
+    elif year:
+        files = files.filter(created_time__year=year)
+
+    paginator = Paginator(files, 100)
+    page_number = request.GET.get('page', 1)
+    files = paginator.get_page(page_number)
+
+    tags = FileTag.objects.filter(
+        webfs_taggedfile_items__content_type=ContentType.objects.get_for_model(PDFUniqueFile),
+        webfs_taggedfile_items__object_id__in=PDFUniqueFile.objects.filter(resource_type=resource_type)
+    ).annotate(file_count=Count('webfs_taggedfile_items')).filter(file_count__gt=0).order_by('-file_count')
+
+    return render(request, 'webfs/uniquefiles/resource_types.html', {
+        'tagged_tags': tagged_tags,
+        'resource_types': PDFUniqueFile.RESOURCE_TYPE_CHOICES,
+        'files': files,
+        'tags': tags,
+        'resource_type': resource_type,
+        'date_filters': date_hierarchy2(PDFUniqueFile.objects.filter(resource_type=resource_type), year, month, day),
+        'file_server': settings.FILE_SERVER,
+    })
+
+
+@login_required
 def duplicates_pdf_files(request, status=None):
     duplicates = PDFUniqueFile.objects
     if status is not None:
@@ -88,8 +153,6 @@ def duplicates_pdf_files(request, status=None):
     duplicates = (duplicates.values('name').annotate(name_count=Count('name'), max_file_size=Max('file_size'))
                   .filter(name_count__gt=1).order_by('-name_count', '-max_file_size')
                   .values('name', 'name_count', 'max_file_size'))
-
-    print(duplicates.query)
 
     paginator = Paginator(duplicates, 100)
     page_number = request.GET.get('page', 1)
